59a73659b0c1955cc20d5ba6ef077a6666a884fc
Zahraa Mathews <zahraa@MacBook-Pro.local>
2017-08-21 23:23:25 +0200

    Add error cases

--- fileReader.js
+++ fileReader.js
@@ -2,8 +2,10 @@ var fs = require('fs');
 exports.fileReader = function(filename) {
     return new Promise((resolve, reject) => {
         fs.readFile(filename, 'utf8', function(err, data) {
+        	if(err) {
+            	reject("Error occured while reading file.")
+        	}
             resolve(data)
-            reject("Error occured while reading file.")
         });
     });
 };
--- main.js
+++ main.js
@@ -14,23 +14,27 @@ var main = function() {
         tweetData = response[1];
         parseUserData(userData);
         displayTweets();
-    }).catch(function(error){
-    	console.log(error);
+    }).catch(function(error) {
+        console.log(error);
     });
 };
 var parseUserData = function(rawText) {
     //split data on newline character 
     var rawUserData = rawText.split(/\r?\n|\r/g);
     for (var index = 0; index < rawUserData.length; index++) {
-        //split account name and follwers
-        var accountInformation = rawUserData[index].split('follows');
-        //add current user
-        addAccount(accountInformation[0].trim(),  accountInformation[1]);
-        //create list of accounts mentioned
-        var users = accountInformation[1].split(',');
-        for (var userIndex = 0; userIndex < users.length; userIndex++) {
-            //attempt to add account and followers 
-            addAccount(users[userIndex],"");
+        if (rawUserData[index].includes('follows')) {
+            //split account name and follwers
+            var accountInformation = rawUserData[index].split('follows');
+            //add current user
+            addAccount(accountInformation[0].trim(), accountInformation[1]);
+            //create list of accounts mentioned
+            var users = accountInformation[1].split(',');
+            for (var userIndex = 0; userIndex < users.length; userIndex++) {
+                //attempt to add account and followers 
+                addAccount(users[userIndex], "");
+            }
+        } else {
+            throw 'File does not adhere to expected format.'
         }
     }
 };
@@ -42,12 +46,12 @@ var addAccount = function(username, followsInfo) {
         accountFollows[username.trim()] = [username.trim()];
     }
     if (followsInfo !== "") {
-    	//update account follows
-        accountFollows[username.trim()] = [username.trim()].concat(followsInfo.replace(' ', '').split(','));
+        //update account follows
+        accountFollows[username.trim()] = [username.trim()].concat(followsInfo.replace(/\s/g, '').split(','));
     }
 };
 var displayTweets = function() {
-	accounts.sort(); 
+    accounts.sort();
     for (var index = 0; index < accounts.length; index++) {
         console.log(accounts[index]);
         //send list of accounts the selected account follows
@@ -58,12 +62,16 @@ var getUserTweets = function(users) {
     var tweets = tweetData.split(/\r?\n|\r/g);
     //iterate through all tweets 
     for (var index = 0; index < tweets.length; index++) {
-    	//get username of tweet
-        var tweetsUser = tweets[index].substring(0, tweets[index].indexOf('>'));
-        var tweet = tweets[index].substring(tweets[index].indexOf('>')+2, tweets[index].length);
-        //check if user is followed by selected account
-        if (users.includes(tweetsUser)) {
-            console.log('@' + tweetsUser + ': ' + tweet.substring(0,140));
+        if (tweets[index].includes('>')) {
+            //get username of tweet
+            var tweetsUser = tweets[index].substring(0, tweets[index].indexOf('>'));
+            var tweet = tweets[index].substring(tweets[index].indexOf('>') + 2, tweets[index].length);
+            //check if user is followed by selected account
+            if (users.includes(tweetsUser)) {
+                console.log('@' + tweetsUser + ': ' + tweet.substring(0, 140));
+            }
+        } else {
+            throw 'File does not adhere to expected format.'
         }
     }
 };
